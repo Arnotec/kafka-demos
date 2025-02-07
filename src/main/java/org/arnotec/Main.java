08e70ae11f1ebcbd07402ae4b866c31a51be7d72
Arnaud <p.tsofack.megniguim@accenture.com>
2025-02-07 12:51:41 +0400

    [Git init] Setting Up Java Maven project with SLF4J

---
+++ src/main/java/org/arnotec/Main.java
@@ -0,0 +1,14 @@
+package org.arnotec;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Test de la log en mode INFO");
+//        System.out.println("Hello, World!");
+
+    }
+}
