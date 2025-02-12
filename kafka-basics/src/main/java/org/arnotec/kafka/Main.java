65487c577d5c55ab500f21402d2cf0032b4e5970
Arnaud <p.tsofack.megniguim@accenture.com>
2025-02-12 13:48:01 +0400

    [Modular project] Refactoring project into modules

---
+++ kafka-basics/src/main/java/org/arnotec/kafka/Main.java
@@ -0,0 +1,13 @@
+package org.arnotec.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Test de l'architecture en modules");
+
+    }
+}
